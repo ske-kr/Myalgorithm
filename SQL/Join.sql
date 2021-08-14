44009d848be42d290da983fca8af851f43114e50
ske-kr <54828276+ske-kr@users.noreply.github.com>
2021-08-14 12:16:53 +0900

    sql

--- SQL/Join.sql
+++ SQL/Join.sql
@@ -17,3 +17,13 @@ ORDER BY INS.DATETIME
 
 -- 있었는데요 없었습니다 문제
 -- 마찬가지로 ON의 활용문제이고, 해당문제는 공통부분만 출력하면 되므로 JOIN을 활용했다.
+
+SELECT INS.NAME,INS.DATETIME
+FROM ANIMAL_INS INS
+LEFT JOIN ANIMAL_OUTS OUTS
+ON INS.ANIMAL_ID = OUTS.ANIMAL_ID
+WHERE OUTS.ANIMAL_ID IS NULL
+ORDER BY DATETIME LIMIT 3
+
+-- left, right, inner join 모두 on id=id형태를 기억하도록 하자!
+-- 여기서 처음보는 것은 LIMIT 3이라는 조건절이고 이는 정렬 후 상위 3개 값만 추출한다는 것이다.
--- SQL/Select.sql
+++ SQL/Select.sql
@@ -17,3 +17,9 @@ ORDER BY HOUR(DATETIME)
 
 -- 알고보니 HOUR같은것도 있었다.. SET 명령어를 활용한 변수를 이용한 그룹화는 아래예시와 같이 활용가능하다
 
+SELECT ANIMAL_ID,NAME
+FROM ANIMAL_INS
+WHERE INTAKE_CONDITION !="Aged"
+ORDER BY ANIMAL_ID;
+
+-- != 와 <>의 차이점이 뭘까 - 동일하다고 한다 . 다만 <>이 dbms공통적으로 다쓰기때문에 <>를 쓰자 정도?
